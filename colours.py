fa2c8e2901185d46cf452214781f735397bb807c
Quentin <Coulis1990@gmail.com>
2018-07-11 17:39:33 +0200

    Changed the colours

--- colours.py
+++ colours.py
@@ -1,8 +1,8 @@
 import libtcodpy as libtcod
 
 colours = {
-    'dark_wall': libtcod.Color(0, 0, 100),
-    'dark_ground': libtcod.Color(50, 50, 150),
-    'light_wall': libtcod.Color(130, 110, 50),
-    'light_ground': libtcod.Color(200, 180, 50)
+    'dark_wall': libtcod.darkest_grey,
+    'dark_ground': libtcod.darker_grey,
+    'light_wall': libtcod.grey,
+    'light_ground': libtcod.light_grey
 }
